0a3acc2419a90c8cbdb5b4cbc492acb2b15a5a40
Geiler Elias Radillo Sarmiento <geilerelias@gmail.com>
2022-04-14 16:01:35 -0500

    actualizaciones pendientes

---
+++ resources/js/components/GoodPracticesComponent.vue
@@ -0,0 +1,105 @@
+<template>
+    <v-container>
+        <v-sheet class="rounded-0">
+            <div class="text-center mb-6">
+                <h2 class="text-h6 text-lg-h5 secondary--text font-weight-black monserrat-text text-uppercase">
+                    BUENAS PRACTICAS
+                </h2>
+                <v-divider class="primary mx-auto mb-6" style="max-width: 28px;"></v-divider>
+            </div>
+
+            <v-container>
+                <v-row>
+                    <v-col class="col-md-6 col-12">
+                        <v-container class="pa-0">
+                            <v-row class="no-gutters justify-start">
+                                <v-col cols="12" class="col-sm-2">
+                                    <div class="text-h2 success--text monserrat-text font-weight-bold pr-8">
+                                        01
+                                    </div>
+                                </v-col>
+                                <v-col cols="12" class="col-sm-9">
+                                    <div
+                                        class="text-body-2 font-weight-regular monserrat-text mb-1 text-left grey--text">
+                                        backed up
+                                    </div>
+                                    <h3 class="text-uppercase text-h6 monserrat-text font-weight-bold mb-1 text-left">
+                                        Respaldo
+                                    </h3>
+
+                                    <v-divider class="primary mr-auto mb-6"
+                                               style="max-width: 28px;">
+                                    </v-divider>
+                                    <p class="base-body text-body-1 monserrat-text grey--text text--darken-1 text-left mb-6">
+                                        Estás respaldado por la asistencia al viajero de Colasistencia.
+                                    </p>
+                                    <div class="my-1">
+                                        <div class="v-sheet mx-auto surface">
+                                            <img
+                                                :src="imgsBuenasPracticas"/>
+                                        </div>
+                                    </div>
+                                </v-col>
+
+                            </v-row>
+                        </v-container>
+                    </v-col>
+                    <v-col class="col-md-6 col-12">
+                        <v-container>
+                            <v-row class="no-gutters justify-start">
+                                <v-col cols="12" class="col-sm-2 ">
+                                    <div class="text-h2 success--text monserrat-text font-weight-bold pr-8 ">
+                                        02
+                                    </div>
+                                </v-col>
+                                <v-col cols="12" class="col col-sm-9 ">
+                                    <div
+                                        class="text-body-2 monserrat-text font-weight-regular mb-1 text-left grey--text">
+                                        training
+                                    </div>
+                                    <h3 class="text-uppercase monserrat-text text-h6 font-weight-bold mb-1 text-left">
+                                        Capacitaciones
+                                    </h3>
+
+                                    <v-divider class="primary  monserrat-text mr-auto mb-6"
+                                               style="max-width: 28px;">
+                                    </v-divider>
+                                    <p class="base-body text-body-1 monserrat-text grey--text text--darken-1 text-left mb-6">
+                                        Nos capacitamos constantemente.
+                                    </p>
+
+                                    <div class="my-4">
+                                        <div>
+                                            <v-img :src="bp"></v-img>
+                                        </div>
+                                    </div>
+                                </v-col>
+
+                            </v-row>
+                        </v-container>
+                    </v-col>
+
+                </v-row>
+            </v-container>
+        </v-sheet>
+    </v-container>
+</template>
+
+<script>
+import bp4 from '../../images/buenas_practicas/buenas-practicas5.png'
+// import bp5 from '../../images/buenas_practicas/buenas-practicas5.png'
+
+import bp from '../../images/buenas_practicas/logos-buenas-practicas turisticas color.png'
+
+export default {
+    name: "GoodPracticesComponent",
+    data: () => ({
+        bp: bp,
+        imgsBuenasPracticas: bp4,
+    })
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/TestimonialsComponent.vue
@@ -0,0 +1,140 @@
+<template>
+    <v-sheet class=" theme--light">
+        <v-container class=" py-4 py-lg-8">
+            <v-row>
+                <v-col class="col-md-4 col-12 fill-height">
+                    <h2 class="text-h6 monserrat-text text-lg-h5 secondary--text font-weight-black text-uppercase">
+                        NUESTROS VIAJEROS NOS RECOMIENDAN
+                    </h2>
+                    <v-divider class="primary  mb-6" style="max-width: 28px;"></v-divider>
+                </v-col>
+            </v-row>
+
+            <v-carousel
+                cycle
+                height="400"
+                hide-delimiter-background
+            >
+                <template v-slot:prev="{ on, attrs }">
+                    <v-btn
+                        color="primary"
+                        icon
+                        v-bind="attrs"
+                        v-on="on"
+                    >
+                        <v-icon>
+                            mdi-chevron-left
+                        </v-icon>
+                    </v-btn>
+                </template>
+                <template v-slot:next="{ on, attrs }">
+                    <v-btn
+                        color="primary"
+                        icon
+                        rounded
+                        v-bind="attrs"
+                        v-on="on"
+                    >
+                        <v-icon>
+                            mdi-chevron-right
+                        </v-icon>
+                    </v-btn>
+                </template>
+                <v-carousel-item
+                    v-for="(item, i) in testimonios"
+                    :key="i"
+                >
+                    <v-sheet
+                        color="transparent"
+                        height="100%"
+                        class="px-12"
+                    >
+                        <v-row
+                            class="fill-height"
+                            align="center"
+                            justify="center"
+                        >
+
+                            <div class="px-12 text-lg-h6 text-body-1 monserrat-text text-center text-justify">
+                                <span class="font-weight-bold">“</span>
+                                {{ item.testimonio }}.
+                                <span class="font-weight-bold">”</span>
+                            </div>
+                            <div class="d-flex align-center justify-center mt-4">
+                                <div class="mr-2 ">
+                                    <v-avatar style="height: 60px; min-width:60px; width: 60px;">
+                                        <v-img :src="item.src" cover>
+                                        </v-img>
+                                    </v-avatar>
+                                </div>
+                                <div class="text-left">
+                                    <div class="text-h6">{{ item.nombre }}</div>
+                                </div>
+                            </div>
+                        </v-row>
+                    </v-sheet>
+                </v-carousel-item>
+            </v-carousel>
+        </v-container>
+
+    </v-sheet>
+</template>
+
+<script>
+import t1 from '@/../images/testimonios/foto-perfil-tatiana-chamorro.png'
+import t2 from '@/../images/testimonios/foto-perfil-santiago.png'
+import t3 from '@/../images/testimonios/foto-perfil-diego.png'
+import t4 from '@/../images/testimonios/foto-perfil-andres-errano.png'
+
+export default {
+    name: "TestimonialsComponent",
+    data: () => ({
+        colors: [
+            'indigo',
+            'warning',
+            'pink darken-2',
+            'red lighten-1',
+            'deep-purple accent-4',
+        ],
+        slides: [
+            'First',
+            'Second',
+            'Third',
+            'Fourth',
+            'Fifth',
+        ],
+        testimonios: [
+            {
+                src: t1,
+                nombre: 'Tatiana C',
+                testimonio: 'Super recomendado excelente atención la calidad humana se siente hacen que nuestro recorrido sea más placentero un acompañamiento no solo del conductor y guía si no de su gerente desde que uno sale hasta que regresa está al pendiente de que las atenciones sean las mejores. @almaguajiratour cumplen con el destino que prometen no se pegan si en el camino hay que conocer algo extra súper los precios las comidas 😋😋😋😋😋😋 Los felicito quedé muy agradecida con sus atenciones excelentes vehículos cómodos en excelentes estados full aire',
+                estrellas: 5
+            },
+            {
+                src: t4,
+                nombre: 'andres s',
+                testimonio: 'Realice este viaje a inicios de enero y fue una experiencia increíble, buena atención, excelente transporte, el alojamiento y alimentación también fueron excelentes y aparte el trato que te brindan es demasiado bueno. Les recomiendo almaguajiratours para realizar esta experiencia tan reconfortante, sin duda alguna los volvería a escoger y lo mejor es que te brindan facilidad de pago. LOS MEJORES',
+                estrellas: 5
+            },
+            {
+                src: t2,
+                nombre: 'Santiago c',
+                testimonio: 'Lo recomiendo ampliamente: seriedad, cumplimiento, responsabilidad, carisma, conocimiento. El tour por la Alta Guajira es inolvidable, una experiencia que hay que vivir. Raguis y Sharkie son excelentes guías. 👏👌',
+                estrellas: 5
+            },
+            {
+                src: t3,
+                nombre: 'Diego C',
+                testimonio: 'Una gran experiencia, la agencia es super recomendada y el servicio de Raguis y Jui fue sensacional… ellos dos marcaron la diferencia.',
+                estrellas: 5
+            }
+        ]
+    })
+}
+</script>
+
+<style>
+.v-window__next, .v-window__prev {
+    background: none !important;
+}
+</style>
